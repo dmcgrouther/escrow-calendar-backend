6fa45ccccba3a19bf61d60a66add81e393ffacd6
david-mcgrouther <dbmcgrouther@gmail.com>
2021-04-12 11:18:00 -0700

    got server.js running

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const PORT = 4000;
+
+// const routes = require('./routes');
+
+// app.use('/api/v1/auth', routes.auth);
+// app.use('/api/v1/users', routes.users);
+// app.use('/api/v1/house', routes.house);
+
+app.listen(PORT, () => console.log(`Server connected at http://localhost:${PORT}... ITS WORKING!!!!!`));
